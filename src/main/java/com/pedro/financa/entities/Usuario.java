ed334a140f2e17b91a24dbcc603e32a7847d4ed5
Pedro Caua <pedro.cauasalgado@gmail.com>
2024-11-21 12:00:59 -0300

    Entidade Usuario e seu recurso

---
+++ src/main/java/com/pedro/financa/entities/Usuario.java
@@ -0,0 +1,75 @@
+package com.pedro.financa.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Usuario implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	private String email;
+	private String senha;
+	
+	// falta fazer associações, por ser primeira entidade ainda não tem
+	
+	public Usuario() {
+	}
+
+	public Usuario(Long id, String name, String email, String senha) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.senha = senha;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		return Objects.equals(id, other.id);
+	}
+}
